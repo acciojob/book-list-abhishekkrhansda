73a0c9201a1a54a832e510be8b4268ea7221269b
abhishek kumar hansda <118197040+abhishekkrhansda@users.noreply.github.com>
2026-09-13 17:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let isbn = document.querySelector("#isbn");
+let btn = document.querySelector("#submit");
+let t = document.querySelector(".table");
+
+btn.addEventListener("click",() => {
+	let row = document.createElement("tr");
+	row.innerHTML = `
+	<td>${title.value}</td>
+	<td>${author.value}</td>
+	<td>${isbn.value}</td>
+	<td><button class="clear">clear</button></td>`
+
+	t.appendChild(row);
+
+	title.value = "";
+	author.value ="";
+	isbn.value = "";
+	
+})
+
+t.addEventListener("click",(e) =>{
+	if(e.target.classList.contains("clear")){
+		e.target.parentElement.parentElement.remove();
+	}
+})
